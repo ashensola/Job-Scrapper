3374e08c76d8344322cbfef190258786af35c721
Ashen <ashensola@gmail.com>
2022-03-02 00:11:43 +1100

    final

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 
 require 'nokogiri'
 require 'httparty'
+require 'sqlite3'
+DB = {:conn => SQLite3::Database.new("../db/favourites.db")}
 
+
+require_relative '../lib/job_search_engine/mydatabase.rb'
 require_relative '../lib/job_search_engine/scrapper.rb'
 require_relative '../lib/job_search_engine/cli.rb'
