2ed13ff2e716cd4e82b49e0328538fa9fca07841
Ashen <ashensola@gmail.com>
2022-03-02 00:12:29 +1100

    finnal commit

--- lib/job_search_engine/cli.rb
+++ lib/job_search_engine/cli.rb
@@ -4,18 +4,31 @@ class CLI
 @inputAge
 @inputPage = 0
 @returnBool
+@newSearch
+@newdatabaseInstance
+
+ attr_accessor :title, :companynameVal, :companylocationVal, :salary
+
+def initialize()
+ @showDatabase = ViewDatabase.new()
+end
+
+
 def welcomeMessage
     puts "Hi , welcome to the Indeed Job Search Engine"  
     self.commandMenu
 end
 
 def commandMenu
-    puts "Please enter a Job"
+    puts "Do you want to search a job (1) or view saved jobs (2)"
+    menuInput = gets.strip.to_i
+if  menuInput == 1
+    puts "Please enter a Job #{@menuInput}"
     @inputJob = gets.strip
 
     puts "Please enter a location"
     @inputLocation= gets.strip
-    puts "Press 1 for jobs from 24 hours, 2 for 3 days, 3 for 7 days and 4 for 14 days"
+    puts "Press 1 for jobs from 24 hours, 3 for 3 days, 7 for 7 days and 14 for 14 days"
     @inputAge=gets.strip.to_i
     puts "Enter a page number"
     @inputPage=gets.strip.to_i
@@ -23,12 +36,38 @@ def commandMenu
     if @returnBool == true
         self.searchAnotherJob
     end
+elsif menuInput == 2
+   
+    @showDatabase.showTable
+
+    puts "Do you want to delete any row (y/n)"
+    inputDelete = gets.strip.downcase
+    if inputDelete == "y"
+        puts "Enter ID that needs to be deleted"
+        getID= gets.strip.to_i
+        @showDatabase.deleteRow(getID)
+        @showDatabase.showTable
+      
+        self.commandMenu
+    elsif inputDelete == "n"
+        self.commandMenu
+
+    else
+        puts "invalid input"
+        self.commandMenu
+    end
+else 
+    puts "invalid input"
+    self.commandMenu
 end
+end
+
+
 
 
 def searchAnotherJob
     if @returnBool != false
-        puts "Do you want to search for another job or choose another page or exit(j/p/e)"
+        puts "Do you want to search for another job, save job, choose another page or exit(j/s/p/e)"
         getInput = gets.strip.downcase
         if getInput == "j"
             self.commandMenu
@@ -43,11 +82,17 @@ def searchAnotherJob
         elsif getInput == "e"
         puts  "Thank you for using the Job Search App"
             exit
-        
-        else 
+        elsif getInput == "s"
+            puts "Enter index of job you want to save"
+            getIndex=gets.strip.to_i
+            selectDatabase(getIndex)
+          
+          
+         else 
             puts "Please enter a valid response"
             self.searchAnotherJob
-        end
+        
+         end
     else
     puts "Please enter a valid input"
     self.commandMenu
@@ -59,12 +104,27 @@ end
 
 def searchJob(inputPage = @inputPage)
    
-   newSearch=Scrapper.new(@inputJob,@inputLocation,@inputPage,@inputAge);
-   @returnBool= newSearch.getJobData?
+   @newSearch=Scrapper.new(@inputJob,@inputLocation,@inputPage,@inputAge);
+   @returnBool= @newSearch.getJobData?
+end
+
+def selectDatabase(index)
+
+    @newSearch.addtoDatabase(index)
+    
+end
+
+
+# def build(newSearch)
+# cli=CLI.new()
+# cli.title=@newSearch.titleVal
+# puts cli.title
+# end
+
+
 
 end
 
-end
 
 newCustomer=CLI.new()
 newCustomer.welcomeMessage
